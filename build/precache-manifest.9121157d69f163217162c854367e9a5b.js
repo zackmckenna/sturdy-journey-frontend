d8de42b8d22e5ddd0e57f1eaf54d5ec4b09ecb0f
Zack McKenna <zackmckenna@gmail.com>
2019-12-17 16:42:16 -0500

    prod build

---
+++ build/precache-manifest.9121157d69f163217162c854367e9a5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a7c189d84d77a2173bc69eb97cd297",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65c9adfe1c4e2c45477a",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "b1b748214fd9fcb02c26",
+    "url": "/static/css/main.db4dff84.chunk.css"
+  },
+  {
+    "revision": "65c9adfe1c4e2c45477a",
+    "url": "/static/js/2.22d280e3.chunk.js"
+  },
+  {
+    "revision": "b1b748214fd9fcb02c26",
+    "url": "/static/js/main.1d480dd7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
